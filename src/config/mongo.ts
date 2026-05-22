4cb3894fa3489331b74090f8a9389d89230d53fd
Renrawin <ren6rst.work@gmail.com>
2026-05-22 15:23:52 +0700

    Update mongo.ts

--- src/config/mongo.ts
+++ src/config/mongo.ts
@@ -2,8 +2,10 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
     try {
-        const uri = 'mongodb://host.docker.internal:27017/log-app';
-        console.log(`⏳ Connecting to MongoDB via host.docker.internal: ${uri}`);
+        // 🌟 กลับมาใช้ชื่อตู้ 'mongodb' เพื่อให้คุยกันในเน็ตเวิร์กของ Docker เองได้เลย
+        const uri = process.env.MONGO_URI || 'mongodb://mongodb:27017/log-app';
+        
+        console.log(`⏳ Connecting inside Docker Network to: ${uri}`);
         
         await mongoose.connect(uri);
         console.log('✅ Connected to MongoDB Successfully!');
